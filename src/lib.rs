fa3d482addb0365f4e58f8fb6a79090120dd9297
GlennFolker <glenn.folk08@gmail.com>
2025-03-19 18:22:25 +0700

    Use `mimalloc-redirect`

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 use std::process::Termination;
 
+use avian2d::PhysicsPlugins;
 use bevy::prelude::*;
+use mimalloc_redirect::MiMalloc;
+
+#[global_allocator]
+static ALLOC: MiMalloc = MiMalloc;
 
 #[unsafe(no_mangle)] // Safety: Only one `android_main` is defined.
 #[cfg(target_os = "android")]
@@ -15,14 +20,31 @@ extern "C" fn main_rs() {
     _ = run();
 }
 
+#[derive(Component, Copy, Clone, Default)]
+#[require(Camera2d)]
+pub struct PrimaryCamera;
+
+#[inline]
 pub fn run() -> impl Termination {
     App::new()
         .add_plugins((
-            DefaultPlugins,
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Centripetal".into(),
+                    ..default()
+                }),
+                ..default()
+            }),
+            PhysicsPlugins::default(),
             hephae::render::<(), ()>(),
             hephae::locales::<(), ()>(),
             hephae::text(),
             hephae::ui::<(), ()>(),
         ))
+        .add_systems(Startup, on_startup)
         .run()
+}
+
+fn on_startup(mut commands: Commands) {
+    commands.spawn(PrimaryCamera);
 }
