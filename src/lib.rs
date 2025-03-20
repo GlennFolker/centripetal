a1261658bb95438a17b268fc59f3508364e0c0bb
GlennFolker <glenn.folk08@gmail.com>
2025-03-20 16:40:58 +0700

    Asset directory redirection

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@ use mimalloc_redirect::MiMalloc;
 
 pub mod persist;
 
+include!(concat!(env!("OUT_DIR"), "/asset_directory.rs"));
+
 #[global_allocator]
 static ALLOC: MiMalloc = MiMalloc;
 
@@ -16,13 +18,18 @@ pub struct PrimaryCamera;
 pub fn run() -> impl Termination {
     App::new()
         .add_plugins((
-            DefaultPlugins.set(WindowPlugin {
-                primary_window: Some(Window {
-                    title: "Centripetal".into(),
+            DefaultPlugins
+                .set(AssetPlugin {
+                    file_path: ASSET_DIRECTORY.into(),
+                    ..default()
+                })
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Centripetal".into(),
+                        ..default()
+                    }),
                     ..default()
                 }),
-                ..default()
-            }),
             PhysicsPlugins::default(),
             hephae::render::<(), ()>(),
             hephae::locales::<(), ()>(),
