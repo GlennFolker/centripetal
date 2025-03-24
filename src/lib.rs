fce66b1e37dfcc4ac23f0f6c4003a34425723b1f
GlennFolker <glenn.folk08@gmail.com>
2025-03-24 22:23:30 +0700

    Update Hephae

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use avian2d::prelude::*;
-use bevy::{log::LogPlugin, prelude::*};
+use bevy::prelude::*;
+use hephae::prelude::*;
 #[cfg(not(target_family = "wasm"))]
 use mimalloc_redirect::MiMalloc;
 
@@ -8,6 +9,7 @@ use mimalloc_redirect::MiMalloc;
 static ALLOC: MiMalloc = MiMalloc;
 
 pub mod persist;
+pub mod player;
 
 #[derive(Component, Copy, Clone, Default)]
 #[require(Camera2d)]
@@ -16,17 +18,9 @@ pub struct PrimaryCamera;
 #[cfg_attr(target_family = "wasm", wasm_bindgen::prelude::wasm_bindgen(start))]
 pub fn run() {
     App::new()
-        .add_plugins((
-            DefaultPlugins.build().add_after::<LogPlugin>(|_: &mut App| {
-                #[cfg(not(target_family = "wasm"))]
-                info!("Using MiMalloc {}", MiMalloc::get_version())
-            }),
-            PhysicsPlugins::default(),
-            hephae::render::<(), ()>(),
-            hephae::locales::<(), ()>(),
-            hephae::text(),
-            hephae::ui::<(), ()>(),
-        ))
+        .add_plugins((DefaultPlugins, PhysicsPlugins::default(), hephae! {
+            ..
+        }))
         .add_systems(Startup, on_startup)
         .run();
 }
